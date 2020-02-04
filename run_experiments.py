2d7df0296373d3424e9205064baad646f9d53f8b
Daniel Khashabi <danielk@allenai.org>
2020-02-04 13:29:00 -0500

    minor.

--- run_experiments.py
+++ run_experiments.py
@@ -65,8 +65,6 @@ def main():
                 output_metrics[dataset + 'loss'] = metrics['loss']
 
     os.mkdir("/output")
-    os.mkdir("output")
-
 
     with open('output/metrics.json', 'w') as f:
         json.dump(output_metrics, f)
