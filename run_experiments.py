5461b201ee4a5b95a1c524a38fd54633f09e0cac
Daniel Khashabi <danielk@allenai.org>
2020-02-04 10:51:54 -0500

    minor change to the output folder of metrics.

--- run_experiments.py
+++ run_experiments.py
@@ -61,7 +61,7 @@ def main():
                 output_metrics[dataset + 'f1'] = metrics['f1']
                 output_metrics[dataset + 'loss'] = metrics['loss']
 
-    with open('metrics.json', 'w') as f:
+    with open('output/metrics.json', 'w') as f:
         json.dump(output_metrics, f)
 
 
